c0feec435ddf40a19a5dda3d5f53f8cad7c8c727
Guido Tello <lpmaleinn@gmail.com>
2023-06-10 20:54:28 -0300

    Update CyberRoach script
    Implemented some interfaces

--- Assets/Scripts/CyberRoach.cs
+++ Assets/Scripts/CyberRoach.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class CyberRoach : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    public CyberRoach()
     {
-        
+        behavior = BehaviorType.Follower;
+
+        hitPoints = 10;
+        attack = 2;
+        def = 2;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void GetHit(int amount)
     {
-        
+        hitPoints -= amount/* *100/def */;
+    }
+    public override void Hit(Entity entity)
+    {
+        //Do something
+    }
+    public override void PowerHit(Entity entity)
+    {
+        //Do something
     }
 }
