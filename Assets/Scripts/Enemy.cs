b4fc7f67a15d04ce1e953f4cc634861f83b76436
Guido Tello <lpmaleinn@gmail.com>
2023-06-10 20:53:46 -0300

    Update Enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,24 @@
-public abstract class Enemy : Entity
+public abstract class Enemy : Entity, ICombatable, IHittable
 {
-    
+    protected int hitPoints;
+    protected int attack;
+    protected int def;
+
+    protected BehaviorType behavior;
+    public int HP { get => hitPoints; }
+    public int ATK { get => attack; }
+    public int DEF { get => def; }
+
+    public virtual void GetHit(int amount)
+    {
+        hitPoints -= amount/* *100/def */;
+    }
+    public virtual void Hit(Entity entity)
+    {
+        //Do something
+    }
+    public virtual void PowerHit(Entity entity)
+    {
+        //Do something
+    }
 }
